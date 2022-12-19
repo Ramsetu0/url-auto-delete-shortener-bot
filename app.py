f0a4749c8019128b95930f7bf6aa69524f43bbfe
Ramsetu0 <119653384+Ramsetu0@users.noreply.github.com>
2022-12-19 18:40:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://ramsetu0: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@github.com/ramsetu0/mogenius ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://ramsetu0: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@github.com/ramsetu0/mogenius ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 -m zaid &")
